f5f5aa0979f501371d0df3897049764c58936a51
Aubrey G <dearaubrey1@gmail.com>
2019-11-05 20:16:04 -0600

    I hope to god this works. :)

---
+++ src/java/CSProject/Database.java
@@ -0,0 +1,105 @@
+package CSProject;
+
+
+import java.sql.ResultSetMetaData;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.json.simple.JSONArray;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class Database {
+    String sessionID;
+    private Connection getConnection() {
+        Connection conn = null;
+        try {
+            
+            Context envContext = new InitialContext();
+            Context initContext  = (Context)envContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource)initContext.lookup("jdbc/db_pool");
+            conn = ds.getConnection();
+            
+        }   
+        
+        catch (SQLException | NamingException e) {}
+        
+        return conn;
+
+    }
+    public String getQueryResults(String sessionID) {
+        this.sessionID = sessionID;
+        StringBuilder table = new StringBuilder();
+        String query;
+        
+        Connection conn = getConnection();
+        query = "SELECT * FROM registrations r WHERE sessionid = ?;";
+        try {
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, sessionID);
+            boolean hasResults = statement.execute();
+            
+            if (hasResults) {
+                ResultSet resultset = statement.getResultSet();
+                table.append("<table>");
+                while (resultset.next()) {
+                 table.append("<tr>");
+                 table.append("<td>").append(resultset.getString("id")).append("</td>");
+                 table.append("<td>").append(resultset.getString("firstname")).append("</td>");
+                 table.append("<td>").append(resultset.getString("lastname")).append("</td>");
+                 table.append("<td>").append(resultset.getString("displayname")).append("</td>");
+                 table.append("<td>").append(sessionID).append("</td>");
+                 table.append("</tr>");
+                }
+                table.append("</table>");
+            }
+        }
+        catch(Exception e){System.err.println(e);}
+        return (table.toString());
+        
+    }
+    public String addRegistration(String fname, String lname, String dname, String sessionid) {
+        String regQuery;
+        String displayname;
+        String id = "";
+        String regCode;
+        ResultSet keys;
+        JSONObject results = new JSONObject();
+        
+        Connection conn = getConnection();
+        regQuery = ("INSERT INTO registrations (firstname, lastname, displayname, sessionid) values (?, ?, ?, ?);");
+        
+        try {
+            PreparedStatement statement = conn.prepareStatement(regQuery, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, fname);
+            statement.setString(2, lname);
+            statement.setString(3, dname);
+            statement.setString(4, sessionid);
+            int bool = statement.executeUpdate();
+            if (bool == 1) {
+                keys = statement.getGeneratedKeys();
+                if (keys.next()) {
+                    id = keys.getString(1);
+                }
+            }
+            
+            regCode = String.format("R" + "%0" + (6-id.length()) + "d%s", 0, id);
+            results.put("displayname", dname);
+            results.put("code", regCode);
+            
+        }
+        catch(Exception e){}
+        finally {
+            return (results.toJSONString());
+        }
+    }
+    
+}
---
+++ src/java/CSProject/Registration.java
@@ -0,0 +1,62 @@
+
+package CSProject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author deara
+ */
+
+@WebServlet(name = "Registration", urlPatterns = {"/registration"})
+public class Registration extends HttpServlet {
+
+    protected void processGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Database db = new Database();
+            String table = db.getQueryResults(request.getParameter("code"));
+            out.println(table);
+        }
+    }
+    
+    protected void processPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        String code = request.getParameter("code");
+        String[] args = code.split(";");
+        String fname = args[0];
+        String lname = args[1];
+        String dname = args[2];
+        String sessionid = args[3];
+        Database db = new Database();
+        String result = db.addRegistration(fname, lname, dname, sessionid);
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println(result);
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processGet(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processPost(request, response);
+    }
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
